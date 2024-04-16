52c7b97dfdcc13d066139beaab6ca9ef2b90a75f
Nicoll-Cruz <ncruzvillegas@frba.utn.edu.ar>
2024-04-16 21:09:54 +0000

    Etapa 3 terminado

--- client/src/client.c
+++ client/src/client.c
@@ -42,9 +42,9 @@ int main(void)
 
 	leer_consola(logger);
 	
-	/*char *linea;
+	//char *linea;
 	
-	/*----------------------------------------------------------PARTE 3-------------------------------------------------------------*/
+	//----------------------------------------------------------PARTE 3-------------------------------------------------------------
 
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
@@ -52,7 +52,7 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
-
+	enviar_mensaje(valor, conexion);
 	// Armamos y enviamos el paquete
 	paquete(conexion);
 
@@ -111,11 +111,22 @@ void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
 	char* leido;
-	t_paquete* paquete;
-
+	t_paquete* paquete= crear_paquete();
+	leido = readline("> ");
 	// Leemos y esta vez agregamos las lineas al paquete
+	while(strcmp(leido, "")!= 0){
+		agregar_a_paquete(paquete, leido, strlen(leido)+1);
+		free(leido);
+		leido = readline("> ");
+	}
+
+
+	enviar_paquete(paquete, conexion);
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+
+	free(leido);
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
@@ -124,4 +135,5 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 	  log_destroy(logger);
 	  config_destroy(config);
+	  liberar_conexion(conexion);
 }
